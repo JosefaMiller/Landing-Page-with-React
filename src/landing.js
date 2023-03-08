675a993ed7af6a83ce144c4d571ed8fcb0b79e79
Josefa Miller <josefamillerb@gmail.com>
2023-03-08 20:13:28 -0300

    agregando estilos

--- src/landing.js
+++ src/landing.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Card from "./card";
 import Navbar from "./navbar";
-import Jumbotron from "./travesti";
+import Jumbotron from "./jumbotron";
 export default function Landing() {
     
     return <main>
         <Navbar/>
+        <div className="container">
         <Jumbotron/>
         <div className="row">
+        <Card/>
         <Card/>
         <Card/>
         <Card/>
